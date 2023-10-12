2201bc3c3770b9d2f390b7e3f1a4a68703e8ca62
viktoriamoksheninova <143103364+viktoriamoksheninova@users.noreply.github.com>
2023-10-12 19:27:55 +0600

    Add files via upload

---
+++ Новая папка/game.py
@@ -0,0 +1,14 @@
+from direct.showbase.ShowBase import ShowBase 
+from hero import Hero
+from mapmanager import Mapmanager
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)        
+        self.land = Mapmanager()
+        x,y = self.land.loadLand('land.txt')
+        self.hero = Hero((x//2,y//2,2),self.land)
+        base.camLens.setFov(90)
+
+game = Game()
+game.run()
